3494feda3209fb6195e1fcf8678692a0b27d59f8
Adib <adibnaj@gmail.com>
2021-10-26 19:55:00 +0200

    Initial Commit

---
+++ PokemonText/PokemonTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTextApp.swift
+//  PokemonText
+//
+//  Created by Adib Najjar on 26/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
